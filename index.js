2e295c1b0514a2a699ad2531e7431d862ded4980
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-12-12 12:29:41 +0100

    Create the searchById function

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-	.then((response) => response.json())
-	.then(json =>console.log(json))
+console.log('Hello World');
+function searchById(){
+	let id = document.getElementById('searchById').value;
+	console.log(id);
+	fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+	.then(response => response.json())
+	.then(json => console.log(json))
+}
+
+
+// searchById(7);
+
+
+
+function searchByText(){
+	
+	let text = document.getElementById('searchText').value;
+	fetch(`https://jsonplaceholder.typicode.com/todos/`)
+	.then(response => response.json())
+	.then(json => {
+		let output =[];
+		for(let index = 0; index < json.length; index++){
+			console.log(json[index]);
+			if(json[index].indexOf(text) !== -1){
+				output.push(json[index]);
+			}
+		}
+		return output;
+	})
+}
+
+// console.log(searchByText(dol)
